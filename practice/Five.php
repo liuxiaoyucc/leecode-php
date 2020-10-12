80fb134c818979a3d4c28f05511775f557949043
xyliu <2199842341@qq.com>
2020-10-12 18:02:49 +0800

    Create Five.php

---
+++ practice/Five.php
@@ -0,0 +1,20 @@
+<?php
+
+class Solution {
+
+	/**
+	 * 寻找最长回文子串
+	 * 回文串: abcba, aba, arera
+	 * @param String $s
+	 * @return String
+	 */
+	function longestPalindrome($s) {
+		echo $s;
+
+	}
+}
+
+$s = new Solution();
+
+$str = 'creadac';
+$s->longestPalindrome($str);
